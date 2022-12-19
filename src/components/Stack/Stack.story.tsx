13a6082d94edf181f0ba67d306983b8b545df5d4
Kyoorim <kyoorim88@gmail.com>
2022-12-19 17:47:36 +0900

    Feat: styled-system 적용 및 OutlineBlack버전 추가

--- src/components/Stack/Stack.story.tsx
+++ src/components/Stack/Stack.story.tsx
@@ -1,11 +1,36 @@
-import React from 'react';
-import { Stack } from './Stack';
+import React, { FC } from 'react';
+import { Meta } from '@storybook/react';
+import Stack, { StackProps } from './Stack';
+
+const data = [
+  { id: 1, text: 'one' },
+  { id: 2, text: 'two' },
+  { id: 3, text: 'three' },
+];
 
 export default {
   title: 'Stack',
   component: Stack,
+} as Meta<StackProps>;
+
+export const Default: FC = () => {
+  return (
+    <>
+      {data.map((el) => (
+        <Stack data-testid={el.id}>{el.text}</Stack>
+      ))}
+    </>
+  );
 };
 
-export const Default = () => {
-  return <Stack></Stack>;
+export const OutlineBlack: FC = () => {
+  return (
+    <>
+      {data.map((el) => (
+        <Stack data-testid={el.id} width={1 / 3} border="1px solid black" borderRadius={10} textAlign="center">
+          {el.text}
+        </Stack>
+      ))}
+    </>
+  );
 };
--- src/components/Stack/Stack.tsx
+++ src/components/Stack/Stack.tsx
@@ -1,24 +1,41 @@
 import React from 'react';
 import styled from 'styled-components';
+// eslint-disable-next-line prettier/prettier
+import {
+  border,
+  BorderProps,
+  color,
+  ColorProps,
+  flexbox,
+  FlexboxProps,
+  layout,
+  LayoutProps,
+  space,
+  SpaceProps,
+  typography,
+  TypographyProps,
+} from 'styled-system';
 
-export const StackBox = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
-export const StackContent = styled.div`
-  border: 1px solid black;
-  width: 50%;
-  text-align: center;
-  border-radius: 5px;
-  padding: 10px 0px;
+const StackBox = styled.div`
+  ${flexbox}
 `;
 
-export const Stack = () => {
+const StackContent = styled.div`
+  ${space}
+  ${layout}
+  ${color}
+  ${border}
+  ${typography}
+`;
+
+export type StackProps = FlexboxProps & SpaceProps & LayoutProps & ColorProps & BorderProps & TypographyProps;
+
+const Stack: FCC<StackProps> = (props: any) => {
   return (
-    <StackBox>
-      <StackContent>1</StackContent>
-      <StackContent>2</StackContent>
-      <StackContent>3</StackContent>
+    <StackBox {...props} data-testid="stackbox-comp">
+      <StackContent {...props} data-testid="stackcontent-comp"></StackContent>
     </StackBox>
   );
 };
+
+export default Stack;
